1be12c0d8f2065b71f0cbf085daaf7e2b3759264
yilmaz <yilmaz@auburn.edu>
2019-03-31 19:42:21 -0500

    Article metadata updated

--- src/main/scala/BiblioAnalysisMain.scala
+++ src/main/scala/BiblioAnalysisMain.scala
@@ -1,4 +1,5 @@
 import scala.io.Source
+import Array._
 
 object BiblioAnalysisMain extends App {
 
@@ -15,25 +16,53 @@ object BiblioAnalysisMain extends App {
   }
 
   def getKeywords(kwds: String) : Array[String] = {
-    kwds.split(";") map(_.stripLeading())
+    var words = kwds.split(";") map(_.stripLeading())
+    if (words(0).startsWith("DE"))
+      words(0)=words(0).substring(3)
+    words
+  }
+
+  def getRef(reference: String) : Ref = {
+    var ref = reference.split(",")
+    var au: String = ""
+    var date: String = ""
+    var src: String = ""
+    if (ref.length > 0) {au  = ref(0)}
+    if (ref.length > 1) {date = ref(1)}
+    if (ref.length > 2) {src  = ref(2)}
+    if (ref.length > 3) {
+      for (j <- 3 until ref.length) src = src + " " + ref(j)
+    }
+    Ref(au,date,src)
   }
 
 
 
-  case class Pub(var authors: String, var title: String, var publication: String)
+  case class Pub(var authors: String, var title: String, var publication: String,
+                 var keywords: Array[String], var refs: List[Ref]) {
+    override def toString(): String = {
+      var str = s"Authors: $authors \n Title: $title \n Source: $publication"
+      if (keywords != null) {keywords foreach {x => {str=str+s"$x\t\t"}}}
+      var count = refs.length
+      str=str+s"\nThere are $count references"
+      str
+    }
+  }
+  case class Ref(var authors: String, var date: String, var source: String)
 
-  println("Contents")
-
-  val src = Source.fromFile("/Users/yilmaz/IdeaProjects/" +
+  val srcData = Source.fromFile("/Users/yilmaz/IdeaProjects/" +
     "BiblioAnalysis/savedrecs.ciw")
   var count : Int = 0
+  var pubs : List[Pub] = List()
 
-  src.mkString split("PT J\n") foreach {x => {
+  var authors : String = null
+  var title : String = null
+  var src : String = null
+  var keywords: Array[String] = null
+  var references : List[Ref] = List()
+
+  srcData.mkString split("PT J\n") foreach {x => {
     println("**********")
-    var authors : String = null
-    var title : String = null
-    var src : String = null
-    var keywords: Array[String] = null
 
     val lines = x split ("\n")
     var currentCode : String = ""
@@ -45,27 +74,34 @@ object BiblioAnalysisMain extends App {
           case "AF" => {authors = getAuthors(l);currentCode="AF"}
           case "TI" => {title = getTitle(l);currentCode="TI"}
           case "SO" => {src = getSource(l);currentCode="SO"}
-          case "DE" =>
+          case "DE" => {keywords = getKeywords(l);currentCode="DE"}
+          case "CR" => {references = getRef(l) :: references;currentCode="CR"}
           case "  " => {
             if (currentCode == "AF") authors = authors + "\n" + l.stripLeading()
-            else
-            if (currentCode == "TI") title = title + " " + l.stripLeading()
-            else
-            if (currentCode == "SO") src = src + " " + l.stripLeading()
+            else if (currentCode == "TI") title = title + " " + l.stripLeading()
+            else if (currentCode == "SO") src = src + " " + l.stripLeading()
+            else if (currentCode == "DE") keywords = concat(keywords,getKeywords(l))
+            else if (currentCode == "CR") references = getRef(l) :: references
           }
           case _ => currentCode = code
           }
-
         }
 
     }
     println(authors)
     println(title)
     println(src)
+    if (keywords != null) {keywords foreach {x => {(print(x)); print("\t\t")}}}
+    println()
+    println("There are "  + references.length + " references")
+    println()
+
   }
+    pubs = Pub(authors,title,src,keywords,references) :: pubs
     count = count+1
   }
 
   println("Number of records is " + count)
+  pubs foreach {p => println(p)}
 
 }
